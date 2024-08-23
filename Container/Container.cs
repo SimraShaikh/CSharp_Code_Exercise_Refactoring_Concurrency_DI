113b27b9edeb72051009286a8a056ea148c4dc03
Simra <simratabassumf@gmail.com>
2024-08-23 09:10:41 -0500

    Implement simple dependency injection container.
    - Added Bind method to associate interfaces with implementations.
    - Added Get method to resolve and instantiate objects based on the bindings.
    
    Add unit tests for Container class.
    
    - Created tests to verify successful binding and resolution of service types.
    - Ensured correct instance retrieval based on interface bindings.

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,36 @@
 using System;
+using System.Collections.Generic;
 
 namespace DeveloperSample.Container
 {
     public class Container
     {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
+        private readonly Dictionary<Type, Type> _bindings = new Dictionary<Type, Type>();
+
+        /// <summary>
+        /// Binds an interface type to an implementation type.
+        /// </summary>
+        /// <param name="interfaceType">The interface type to bind.</param>
+        /// <param name="implementationType">The implementation type to bind to the interface.</param>
+        public void Bind(Type interfaceType, Type implementationType)
+        {
+            _bindings[interfaceType] = implementationType;
+        }
+
+        /// <summary>
+        /// Resolves an instance of the specified type from the container.
+        /// </summary>
+        /// <typeparam name="T">The type to resolve.</typeparam>
+        /// <returns>An instance of the specified type.</returns>
+        /// <exception cref="InvalidOperationException">Thrown when no binding is found for the specified type.</exception>
+        public T Get<T>()
+        {
+            var interfaceType = typeof(T);
+            if (_bindings.TryGetValue(interfaceType, out var implementationType))
+            {
+                return (T)Activator.CreateInstance(implementationType);
+            }
+            throw new InvalidOperationException($"No binding found for {interfaceType.FullName}");
+        }
     }
 }
